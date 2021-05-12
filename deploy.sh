c224ee67f9d745e009493efb6a4bd63bde14461f
V1D1AN <lorenzohades@gmail.com>
2021-05-12 17:28:09 +0200

    Add configuring interface

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,19 @@ mv env.sample .env
 docker-compose up -d elasticsearch
 sleep 45
 docker exec -ti elasticsearch elasticsearch-setup-passwords interactive
-echo "###################################"
-echo "#### ENTER PASSWORD OF ELASTIC ####"
-echo "###################################"
+echo "##########################################"
+echo "###### CONFIGURING ACCOUNT ELASTIC #######"
+echo "##########################################"
 read -s -p "Enter the password of account elastic:" password
 password=$password
 sed -i "s/changeme/$password/g" cortex/application.conf elastalert/elastalert.yaml filebeat/filebeat.yml metricbeat/metricbeat.yml kibana/kibana.yml auditbeat/auditbeat.yml logstash/pipeline/03_output.conf
 sed -i "s/elastic_opencti/$password/g" docker-compose.yml
+echo
+echo "##########################################"
+echo "#### CONFIGURING MONITORING INTERFACE ####"
+echo "##########################################"
+read -s -p "Enter the monitoring interface (ex:ens32):" monitoring_interface
+monitoring_interface=$monitoring_interface
+sed -i "s/monitoring_interface/$monitoring_interface/g" docker-compose.yml
 echo
 docker-compose up -d
