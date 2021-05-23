1e0d5ef5f545a335795dee2eb70dd682451d5b23
V1D1AN <lorenzohades@gmail.com>
2021-05-23 13:41:58 +0200

    Add the update of yara rules

--- deploy.sh
+++ deploy.sh
@@ -51,4 +51,17 @@ echo "##########################################"
 echo
 docker exec -ti suricata suricata-update update-sources
 docker exec -ti suricata suricata-update --no-test
-
+echo
+echo "##########################################"
+echo "########## UPDATE YARA RULES #############"
+echo "##########################################"
+echo
+mkdir tmp
+git clone https://github.com/Yara-Rules/rules.git tmp
+rm -fr stoq/rules/yara/*
+rm tmp/malware/MALW_AZORULT.yar
+mv tmp/* stoq/rules/yara/
+rm -fr tmp
+cd stoq/rules/yara
+bash index_gen.sh
+docker restart stoq
