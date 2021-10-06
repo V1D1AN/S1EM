e33db8519ae699e9618e960a0d17ae1693973562
V1D1AN <v1d1an.github@gmail.com>
2021-10-06 14:51:44 +0200

    Change mwdb to plugin archiver

--- 02_deploy_api_key.sh
+++ 02_deploy_api_key.sh
@@ -3,7 +3,7 @@ echo "#################################################################"
 echo "### SCRIPT FOR DEPLOYEMENT OF API KEY FOR MISP/THEHIVE/CORTEX ###"
 echo "#################################################################"
 echo ""
-read -r -p "Have you got yours API KEY of MISP/THEHIVE/CORTEX  ? (Y/N)" input
+read -r -p "Have you got yours API KEY of MISP/THEHIVE/CORTEX/MWDB  ? (Y/N)" input
 
 case $input in
         [yY][eE][sS]|[yY]) 
@@ -16,7 +16,10 @@ case $input in
         read -r -p "Enter the API KEY of MISP:" misp
         misp=$misp
         sed -i "s|misp_api_key|$misp|g" thehive/application.conf cortex/MISP.json filebeat/modules.d/threatintel.yml docker-compose.yml
-        docker-compose restart elastalert filebeat thehive cortex
+		read -r -p "Enter the API KEY of MWDB:" mwdb
+        mwdb=$mwdb
+		sed -i "s|mwdb_api_key|$mwdb|g" stoq/stoq.cfg
+        docker-compose restart elastalert filebeat thehive cortex stoq
         docker-compose up -d connector-misp
         ;;
         [nN][oO]|[nN])
