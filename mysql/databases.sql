52be24fe1a1fb0472e49fdd47e16f4540b5a7a08
V1D1AN <v1d1an.github@gmail.com>
2022-01-30 15:19:36 +0100

    Correction Bug

--- mysql/databases.sql
+++ mysql/databases.sql
@@ -2,5 +2,5 @@
 CREATE DATABASE IF NOT EXISTS `misp`;
 
 # create misp user and grant rights
-CREATE USER 'misp'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'misp'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'misp'@'%';
