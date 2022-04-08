ae40c142894bd2ed96aabedf67c132dbf099620d
V1D1AN <v1d1an.github@gmail.com>
2022-04-08 18:34:15 +0200

    Correction bug of deployment

--- 01_deploy.sh
+++ 01_deploy.sh
@@ -150,8 +150,7 @@ echo "##########################################"
 echo "########## DEPLOY KIBANA INDEX ###########"
 echo "##########################################"
 echo
-echo "Setting kibana_system password";
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD} -H "Content-Type: application/json" https://es01:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
+echo
 while [ "$(docker logs kibana | grep -i "server running" | grep -v "NotReady")" == "" ]; do
   echo "Waiting for Kibana to come online.";
   sleep 15;
